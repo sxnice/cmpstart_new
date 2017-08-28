b907b70739ca28d4c44f82397087209e5b79e501
root <jerry_hu123@163.com>
2017-08-28 12:23:47 +0800

    cmp0828

--- mysqlha_MM/iptablesmysql.sh
+++ mysqlha_MM/iptablesmysql.sh
@@ -15,12 +15,14 @@ ssh  $i <<EOF
 		sed -i /3306/d /etc/iptables
 		sed -i /icmp/d /etc/iptables
 		sed -i /vrrp/d /etc/iptables
+		sed -i /"224.0.0.0"/d /etc/iptables
                 iptables-restore </etc/iptables
 		iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 		iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
 		iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 		iptables -A INPUT -p icmp --icmp-type any -j ACCEPT
 		iptables -A INPUT -i eth0 -p 112 -j ACCEPT
+		iptables -A INPUT -i eth0 -d 224.0.0.0/8 -j ACCEPT
 		iptables -P INPUT DROP
 		exit
 EOF
